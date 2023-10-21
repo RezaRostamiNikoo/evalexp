9e435c337b4cc0db6e638e00985ab6e013594e46
Reza Rostaminikoo RezaRostaminikoo@gmail.com <RezaRostaminikoo@gmail.com>
2023-10-21 14:30:57 -0400

    test some special cases

---
+++ test/specialCases.spec.ts
@@ -0,0 +1,13 @@
+import { parse } from "../src"
+
+describe("special cases", () => {
+
+    test('some special cases', () => {
+
+        expect(parse(12 as unknown as string)).toEqual(12)
+        expect(parse({ a: 1 } as unknown as string)).toEqual({ a: 1 })
+        expect(parse([1] as unknown as string)).toEqual([1])
+
+    })
+
+})
