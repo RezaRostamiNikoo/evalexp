c160b120760541f3b856fbaec6ab188179c28a84
Reza Rostaminikoo RezaRostaminikoo@gmail.com <RezaRostaminikoo@gmail.com>
2023-10-21 13:16:55 -0400

    separate scope and new test for nodes

---
+++ test/data.ts
@@ -0,0 +1,26 @@
+import { IScope } from "../src";
+
+const data = new Map()
+data.set("q", "12")
+data.set("a", "1+2")
+data.set("b", "a")
+data.set("c", "b")
+data.set("hamid", "c * b^2 / q * 4")
+data.set("pp", "3.14")
+data.set("pi", "1.57")
+data.set("oo", {
+    a: 125, b: 20, c: {
+        a: "pp",
+        b: [888888888, 999999999]
+    }
+})
+data.set("aa", [400, 500, 600])
+
+export class MScope implements IScope {
+    get(key: string) { return data.get(key) }
+    getWithArgs(key: string, ...args: any[]) {
+        throw new Error("Method not implemented.");
+    }
+    set(key: string, value: any) { data.set(key, value) }
+    has(key: any): boolean { return data.has(key) }
+}
--- test/nodes.spec.ts
+++ test/nodes.spec.ts
@@ -1,35 +1,113 @@
-import { parse } from "../src";
+import { AccessorNode, ArrayNode, FunctionNode, IndexNode, OperatorNode, ParenthesesNode, RelationalNode, SymbolNode, parse } from "../src";
 import { IScope } from "../src";
-import * as functions from "../src/defaultScopeItems"
-
-const data = new Map();
-data.set("a", "1+2");
-data.set("b", "a");
-data.set("c", "b");
-data.set("hamid", "c");
-// const scope = new Scope(data);
+import { ConstantNode } from "../src"
+import { isPlainObject } from "../src/utils/customs";
+import { MScope } from "./data";
 
 
-describe("Testing the Parser", () => {
 
-    test("simple", () => {
-        // expect(parse("12").evaluate()).toEqual(12)
+const scope = new MScope()
+
+describe("Testing the ExpressionNodes", () => {
+
+    test(`Test for "ConstantNode"`, () => {
+        expect(new ConstantNode("458").evaluate()).toEqual(458)
     })
 
-    test("SymbolNode", () => {
-        // expect(parse("12+3").evaluate(scope)).toEqual(15)
-        // expect(parse("12/3").evaluate(scope)).toEqual(4)
-        // expect(parse("12*3").evaluate(scope)).toEqual(36)
-        // expect(parse("12-3").evaluate(scope)).toEqual(9)
-        // expect(parse("2^3").evaluate(scope)).toEqual(8)
-        // expect(parse("2^-3").evaluate(scope)).toEqual(Math.pow(2, -3))
-        // expect(parse("2e3").evaluate(scope)).toEqual(2e3)
+    test(`Test for "SymbolNode"`, () => {
+        expect(new SymbolNode("q").evaluate(scope)).toEqual(12)
+        expect(new SymbolNode("a").evaluate(scope)).toEqual(3)
+        expect(new SymbolNode("hamid").evaluate(scope)).toEqual(9)
     })
 
-    // test("Functions", () => {
-    //     expect(parse("sin(3.14)").evaluate(scope)).toEqual(Math.sin(3.14))
+    test(`Test for "FunctionNode"`, () => {
+        expect(new FunctionNode("sin", [new SymbolNode("pp")]).evaluate(scope)).toEqual(Math.sin(3.14))
+    })
 
-    // })
+    test(`Test for "AccessorNode"`, () => {
 
+        expect(typeof parse("oo").evaluate(scope)).toEqual("object")
+        expect(isPlainObject(parse("oo").evaluate(scope))).toEqual(true)
+        expect(new AccessorNode(new SymbolNode("oo"), new IndexNode([new SymbolNode("a")], true)).evaluate(scope)).toEqual(125)
+
+        expect(new AccessorNode(
+            new AccessorNode(new SymbolNode("oo"), new IndexNode([new SymbolNode("c")], true)),
+            new IndexNode([new SymbolNode("a")], true)
+        ).evaluate(scope)).toEqual("pp")
+
+        expect(new AccessorNode(
+            new AccessorNode(
+                new AccessorNode(new SymbolNode("oo"), new IndexNode([new SymbolNode("c")], true)),
+                new IndexNode([new SymbolNode("b")], true)
+            ), new IndexNode([new ConstantNode(1)])
+        ).evaluate(scope)).toEqual(999999999)
+
+    })
+
+    test(`Test for "ArrayNode"`, () => {
+
+        expect(new ArrayNode([
+            new ConstantNode(1),
+            new ConstantNode(2),
+            new ConstantNode(3),
+            new ConstantNode(4),
+        ]).evaluate(scope)).toEqual([1, 2, 3, 4])
+
+        expect(new ArrayNode([
+            new ConstantNode(1),
+            new ConstantNode(2),
+            new ConstantNode(3),
+            new ArrayNode([
+                new ConstantNode(8),
+                new ConstantNode(9),
+                new SymbolNode("hamid")
+            ])
+        ]).evaluate(scope)).toEqual([1, 2, 3, [8, 9, 9]])
+    })
+
+    test(`Test for "OperatorNode"`, () => {
+        expect(new OperatorNode("+", "add", [new ConstantNode(2), new ConstantNode(3)])
+            .evaluate(scope)).toEqual(5)
+
+        expect(new OperatorNode("-", "subtract", [new ConstantNode(2), new ConstantNode(3)])
+            .evaluate(scope)).toEqual(-1)
+
+        expect(new OperatorNode("-", "unaryMinus", [new ConstantNode(2)])
+            .evaluate(scope)).toEqual(-2)
+
+    })
+
+    test(`Test for "ParenthesesNode"`, () => {
+        expect(new ParenthesesNode(new ArrayNode([new ConstantNode(3)]))
+            .evaluate(scope)).toEqual([3])
+    })
+
+    test(`Test for "RelationalNode"`, () => {
+        expect(new RelationalNode(
+            [
+                "larger"
+            ],
+            [
+                new ConstantNode(5),
+                new ConstantNode(6)
+            ]
+
+        )
+            .evaluate(scope)).toEqual(false)
+
+        expect(new RelationalNode(
+            [
+                "smaller",
+                "smaller"
+            ],
+            [
+                new ConstantNode(5),
+                new ConstantNode(6),
+                new ConstantNode(10),
+            ]
+
+        )
+            .evaluate(scope)).toEqual(true)
+    })
 
 });
