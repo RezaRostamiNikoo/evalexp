ed7fbb1845f5c1dce72ac33c208ef18374e65ace
Reza Rostaminikoo RezaRostaminikoo@gmail.com <RezaRostaminikoo@gmail.com>
2023-10-28 14:45:44 -0400

    little update

--- src/defaultScopeItems/arithmetic.ts
+++ src/defaultScopeItems/arithmetic.ts
@@ -1,17 +1,21 @@
-export const subtract = (x: number, y: number) => x - y;
-export const sqrt = Math.sqrt;
-export const cbrt = Math.cbrt;
-export const pow = Math.pow;
-export const multiply = (x: number, y: number): number => x * y;
-export const divide = (x: number, y: number): number => x / y;
-export const add = (x: number, y: number): number => x + y;
-export const abs = Math.abs;
-export const ceil = Math.ceil;
-export const floor = Math.floor;
-export const sign = Math.sign;
-export const exp = Math.exp;
-export const max = Math.max;
-export const min = Math.min;
+export const unaryMinus = (number: number) => -1 * number
+export const unaryPlus = (number: number) => number
 
-export const unaryMinus = (number: number) => -1 * number;
-export const unaryPlus = (number: number) => number;
+export const subtract = (x: number, y: number) => x - y
+export const sqrt = Math.sqrt
+export const cbrt = Math.cbrt
+export const pow = Math.pow
+export const multiply = (x: number, y: number): number => x * y
+export const divide = (x: number, y: number): number => x / y
+export const add = (x: number, y: number): number => x + y
+export const abs = Math.abs
+export const ceil = Math.ceil
+export const floor = Math.floor
+export const sign = Math.sign
+export const exp = Math.exp
+export const max = Math.max
+export const min = Math.min
+export const log = Math.log
+
+export const random = Math.random
+export const round = Math.round
--- src/defaultScopeItems/trigonomy.ts
+++ src/defaultScopeItems/trigonomy.ts
@@ -1,5 +1,9 @@
-export const sin = Math.sin;
-export const cos = Math.cos;
-export const tan = Math.tan;
+export const sin = Math.sin
+export const cos = Math.cos
+export const tan = Math.tan
+export const acos = Math.acos
+export const asin = Math.asin
+export const atan = Math.atan
+export const atan2 = Math.atan2
 
 
